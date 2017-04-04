46f4f3e740070e5d75b146e92b586e80a8d2b357
stanislav kumanov <s2501@abv.bg>
2017-04-04 14:05:11 +0300

    js data types

--- js/hello-world.js
+++ js/hello-world.js
@@ -1,1 +1,36 @@
 console.log('hello world');
+
+// ES5
+var paramES5;
+
+// ES6 == ES2015
+let paramES6;
+
+// string
+var stringParam = "I'm string";
+
+// integer
+var integerParam = 5;
+
+// array
+var arrayParam = [4,5,integerParam,'another string'];
+
+// object подобен на css selector с property-та
+var objectParam = {
+    background: 'red',
+    textAlign: 'center',
+    property: integerParam
+};
+
+// малко по сложен object
+var exampleOfTheInternet = {
+    myName: 'Stanislav',
+    gender: 'male',
+    occupation: {
+        example1: 'asd',
+        example2: 'tre',
+        example3: 'as "asd"',
+        example4: "another string"
+    },
+    arryaExample: [1,2,3]
+};
